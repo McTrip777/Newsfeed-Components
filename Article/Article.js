2bfe5b68142806dc27d801cc99e017e145bb7dd3
McTrip777 <mcjakedude@gmail.com>
2019-01-30 14:44:32 -0800

    working on stretch

--- Article/Article.js
+++ Article/Article.js
@@ -6,30 +6,25 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');
-    this.collapseButton = domElement.querySelector('.collapseButton');
+    //this.collapseButton = domElement.querySelector('.collapseButton');
 
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     // this.expandButton.innerHTML = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle())
-    this.collapseButton.addEventListener('click', () => this.expandArticle())
 
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.expandButton.classList.toggle('article-open');
-    this.collapseButton.classList.toggle('article-open');
-    this.collapseButton.classList.toggle('.toggle-on');
-    this.expandButton.classList.toggle('.toggle-on');
+    this.domElement.classList.toggle('article-open');    
   }
 }
 /* START HERE: 
 - Select all classes named ".article" and assign that value to the articles variable.  
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
 */
-let articles = document.querySelectorAll('.article');
-articles.forEach(element => new Article(element));
+let articles = document.querySelectorAll('.article').forEach(element => new Article(element));
 
 
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -11,12 +11,13 @@ const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', toggleMenu);
 
 const hover = document.querySelectorAll('.menu li');
-hover[0].addEventListener('mouseover', e =>{
+for ( i=0; i<hover.length;i++){
+hover[i].addEventListener('mouseover', e =>{
 	e.target.style.color = "white";
 	e.target.style.backgroundColor = "black";
 setTimeout(function() {
   e.target.style.color = "";
   e.target.style.backgroundColor = "";
-
 	}, 1000);
 }, false);
+};
